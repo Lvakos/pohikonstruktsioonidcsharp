208fd0f5da1ad4e1b3f25dda1e6556d60f8300b7
Maksimilian Puhtejev <foxigosi222@gmail.com>
2026-02-19 14:20:00 +0200

    Update Class1.cs

--- Osa3C#/Osa2/Class1.cs
+++ Osa3C#/Osa2/Class1.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Text;
 
-namespace Naidis_IKTpv25.Osa2
+namespace Osa3C_.Osa2
 {
     public class OsaFunktsioonid
     {
